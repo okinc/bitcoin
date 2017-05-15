979facdff79a74badad252f6291f14611e1e3720
zesheng.chen@okcoin.com <zesheng.chen@okcoin.com>
2017-05-15 10:27:28 +0800

    no verification inputmempool

--- src/rpc/rawtransaction.cpp
+++ src/rpc/rawtransaction.cpp
@@ -893,7 +893,7 @@ UniValue sendrawtransaction(const UniValue& params, bool fHelp)
                 result = JSONRPCError(RPC_TRANSACTION_REJECTED, strprintf("%i: %s", state.GetRejectCode(), state.GetRejectReason()));
             } else {
                 if (fMissingInputs) {
-                    result = JSONRPCError(RPC_TRANSACTION_ERROR, "missing inputs");
+                 //   result = JSONRPCError(RPC_TRANSACTION_ERROR, "missing inputs");
                 } else {
                     result = JSONRPCError(RPC_TRANSACTION_ERROR, state.GetRejectReason());
                 }
